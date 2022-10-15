ac32de106c24f823a1b6983c7f656f2e4e1f25c9
mfurkanakin <mfakin@yahoo.com>
2022-10-15 18:58:29 +0300

    edit app.vue imports cookie

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,9 @@
 
 import { ref } from 'vue'
 import Autocomplete from './components/Autocomplete.vue';
+import Cookie from "./components/Cookie.vue";
+
+const feedback = ref();
 
 const productList = [
 	'Apple',
@@ -22,6 +25,13 @@ const selectProduct = ref('');
 
 <template>
 	<div class="container">
+		<Teleport to="body">
+		<Cookie title="Çerezleri kabul et" text="Bizler, güvenliğinize önem veriyor ve bu Çerez Politikası ile siz sevgili ziyaretçilerimizi,
+		 web sitemizde hangi çerezleri, hangi amaçla kullandığımız ve çerez ayarlarınızı nasıl değiştireceğiniz konularında kısaca bilgilendirmeyi hedefliyoruz.
+		 Sizlere daha iyi hizmet verebilmek adına, çerezler vasıtasıyla, ne tür kişisel verilerinizin hangi amaçla toplandığı ve nasıl işlendiği konularında,
+		 kısaca bilgi sahibi olmak için lütfen bu Çerez Politikasını okuyunuz.
+		 Daha fazla bilgi için Gizlilik Politikamıza göz atabilir ya da bizlerle çekinmeden iletişime geçebilirsiniz." v-model:feedback="feedback"></Cookie>
+	</Teleport>
 		<Autocomplete :options="productList" v-model:select="selectProduct" placeholder="Search product"></Autocomplete>
 	</div>
 </template>
@@ -36,23 +46,4 @@ const selectProduct = ref('');
 	background-image: linear-gradient(-45deg, #ffffff, #ffffff 50%, #f6f6f6 50%, #f6f6f6);
 	background-size: 7px 7px;
 }
-
-import Cookie from "./components/Cookie.vue";
-import { ref } from "vue";
-
-const feedback = ref();
-</script>
-
-<template>
-	<Teleport to="body">
-		<Cookie title="Çerezleri kabul et" text="Bizler, güvenliğinize önem veriyor ve bu Çerez Politikası ile siz sevgili ziyaretçilerimizi,
-		 web sitemizde hangi çerezleri, hangi amaçla kullandığımız ve çerez ayarlarınızı nasıl değiştireceğiniz konularında kısaca bilgilendirmeyi hedefliyoruz.
-		 Sizlere daha iyi hizmet verebilmek adına, çerezler vasıtasıyla, ne tür kişisel verilerinizin hangi amaçla toplandığı ve nasıl işlendiği konularında,
-		 kısaca bilgi sahibi olmak için lütfen bu Çerez Politikasını okuyunuz.
-		 Daha fazla bilgi için Gizlilik Politikamıza göz atabilir ya da bizlerle çekinmeden iletişime geçebilirsiniz." v-model:feedback="feedback"></Cookie>
-	</Teleport>
-</template>
-
-<style lang="scss">
-
 </style>
