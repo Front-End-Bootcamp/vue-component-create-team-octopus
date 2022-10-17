17308b2a08fd99b71f187dfd18d23c913851f69f
kaplanark <kaplanarkan@gmail.com>
2022-10-17 18:35:42 +0300

    added popup component

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,14 @@
 <script setup>
-import BootcampLogo from "./assets/svg/BootcampLogo.vue"
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue';
+import Pupup from './components/Pupup.vue';
+
+const showPopup = ref(false);
 </script>
 
 <template>
-	<div>
-		<BootcampLogo />
-		<br />
-		<a href="https://vitejs.dev" target="_blank">
-			<img src="/vite.svg" class="logo" alt="Vite logo" />
-		</a>
-		<a href="https://vuejs.org/" target="_blank">
-			<img src="/vue.svg" class="logo vue" alt="Vue logo" />
-		</a>
-	</div>
-	<HelloWorld msg="Vite + Vue" />
+	<Teleport to="body">
+		<Pupup title="Title" text="Text" v-model:show="showPopup" negative-text="Cancel" positive-text="Ok" closable
+			@positive-click="" @negative-click="">
+		</Pupup>
+	</Teleport>
 </template>
-
-<style scoped>
-.logo {
-	height: 6em;
-	padding: 1.5em;
-	will-change: filter;
-}
-
-.logo:hover {
-	filter: drop-shadow(0 0 2em #646cffaa);
-}
-
-.logo.vue:hover {
-	filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
